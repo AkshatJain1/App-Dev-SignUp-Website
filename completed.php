58f6cb916a0076bffde652d3f9af97730dacba78
Akshat <respectthegame23@gmail.com>
2018-03-13 11:34:32 -0500

    final sign up website done. pushes to sql database with information onsubmit by redirecting to completeded php page. nice looking form created with pertinent information. background 'mobile.jpg' is nice looking background found on google

---
+++ completed.php
@@ -0,0 +1,176 @@
+
+<style type="text/css">
+.form-style-5{
+    max-width: 500px;
+    padding: 10px 20px;
+    background: #f4f7f8;
+    margin: 10px auto;
+    padding: 20px;
+    background: #f4f7f8;
+    border-radius: 8px;
+    font-family: Georgia, "Times New Roman", Times, serif;
+}
+.form-style-5 fieldset{
+    border: none;
+}
+.form-style-5 legend {
+    font-size: 1.4em;
+    margin-bottom: 10px;
+}
+.form-style-5 label {
+    display: block;
+    margin-bottom: 8px;
+}
+.form-style-5 input[type="text"],
+.form-style-5 input[type="date"],
+.form-style-5 input[type="datetime"],
+.form-style-5 input[type="email"],
+.form-style-5 input[type="number"],
+.form-style-5 input[type="search"],
+.form-style-5 input[type="time"],
+.form-style-5 input[type="url"],
+.form-style-5 textarea,
+.form-style-5 select {
+    font-family: Georgia, "Times New Roman", Times, serif;
+    background: rgba(255,255,255,.1);
+    border: none;
+    border-radius: 4px;
+    font-size: 16px;
+    margin: 0;
+    outline: 0;
+    padding: 7px;
+    width: 100%;
+    box-sizing: border-box;
+    -webkit-box-sizing: border-box;
+    -moz-box-sizing: border-box;
+    background-color: #e8eeef;
+    color:#8a97a0;
+    -webkit-box-shadow: 0 1px 0 rgba(0,0,0,0.03) inset;
+    box-shadow: 0 1px 0 rgba(0,0,0,0.03) inset;
+    margin-bottom: 30px;
+    resize: none;
+
+}
+.form-style-5 input[type="text"]:focus,
+.form-style-5 input[type="date"]:focus,
+.form-style-5 input[type="datetime"]:focus,
+.form-style-5 input[type="email"]:focus,
+.form-style-5 input[type="number"]:focus,
+.form-style-5 input[type="search"]:focus,
+.form-style-5 input[type="time"]:focus,
+.form-style-5 input[type="url"]:focus,
+.form-style-5 textarea:focus,
+.form-style-5 select:focus{
+    background: #d2d9dd;
+}
+.form-style-5 select{
+    -webkit-appearance: menulist-button;
+    height:35px;
+}
+.form-style-5 .number {
+    background: #1abc9c;
+    color: #fff;
+    height: 30px;
+    width: 30px;
+    display: inline-block;
+    font-size: 0.8em;
+    margin-right: 4px;
+    line-height: 30px;
+    text-align: center;
+    text-shadow: 0 1px 0 rgba(255,255,255,0.2);
+    border-radius: 15px 15px 15px 0px;
+}
+
+.form-style-5 input[type="submit"],
+.form-style-5 input[type="button"]
+{
+    position: relative;
+    display: block;
+    padding: 19px 39px 18px 39px;
+    color: #FFF;
+    margin: 0 auto;
+    background: #1abc9c;
+    font-size: 18px;
+    text-align: center;
+    font-style: normal;
+    width: 100%;
+    border: 1px solid #16a085;
+    border-width: 1px 1px 3px;
+    margin-bottom: 10px;
+}
+.form-style-5 input[type="submit"]:hover,
+.form-style-5 input[type="button"]:hover
+{
+    background: #109177;
+}
+body{
+
+   background: url(mobile.jpg) no-repeat center center fixed;
+-webkit-background-size: cover;
+-moz-background-size: cover;
+-o-background-size: cover;
+background-size: cover;
+
+}
+</style>
+<body>
+  <div style="margin-top:12em;" class="form-style-5">
+    <h3 style="padding: 3em;"><center>THANKS FOR COMPLETING</center></h3>
+  </div>
+</body>
+
+
+
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "admin";
+$dbname = "app_dev";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+
+$langs = "N/A";
+if (isset($_POST["lang"])) {
+  $langs ="";
+  $name =  $_POST["lang"];
+  foreach ($name as $color){
+      $langs .=$color."|";
+  }
+}
+
+$tshirt = "N/A";
+if(isset($_POST["tshirt"])){
+  $tshirt = "";
+  $tshirt = $_POST["tshirt"];
+}
+
+$platform = "N/A";
+if (isset($_POST["platform"])) {
+  $platform ="";
+  $name =  $_POST["platform"];
+  foreach ($name as $color){
+      $platform .=$color."|";
+  }
+}
+
+
+
+  // $sql = "INSERT INTO members (FirstName, LastName, Username) VALUES('".generateRandomString(6)."','".generateRandomString(12)."','".generateRandomString(18)."');";
+  $sql = "INSERT INTO members(name, email, tshirt, exp, platform, languages, ideas) VALUES ('".$_POST["field1"]."','".$_POST["field2"]."','".$tshirt."','".$_POST["field3"]."','".$platform."','".$langs."','".$_POST["field4"]."');";
+
+  $result = $conn->query($sql);
+
+
+
+
+$conn->close();
+
+
+
+?>
